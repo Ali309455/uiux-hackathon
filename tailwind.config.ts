c22bca08ebbfb335330493b174ec935b1a072cb8
Mohammad Ali <alikhanak012345@gmail.com>
2024-12-18 01:04:29 +0500

    trying to fix issues in vercel deployment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,21 +9,21 @@ export default {
   theme: {
     extend: {
       screens: {
-        "max-1930": { max: "1930px" }, // Applies when width <= 1930px
-        "max-1800": { max: "1800px" }, // Applies when width <= 1800px
-        "max-1625": { max: "1625px" }, // Applies when width <= 1800px
-        "max-1400": { max: "1400px" }, // Applies when width <= 1400px
-        "max-1350": { max: "1350px" },
-        "max-1285": { max: "1285px" },
-        "max-1257": { max: "1257px" },
-        "max-1080": { max: "1080px" }, // Applies when width <= 1080px
-        "max-950": { max: "950px" },
-        "max-802": { max: "802px" },
-        "max-750": { max: "750px" },
-        "max-650": { max: "650px" },
-        "max-550": { max: "550px" },
-        "max-450": { max: "450px" },
-        "max-415": { max: "415px" },
+        "max-1930": "1930px", // Applies when width <= 1930px
+        "max-1800": "1800px", // Applies when width <= 1800px
+        "max-1625": "1625px", // Applies when width <= 1625px
+        "max-1400": "1400px", // Applies when width <= 1400px
+        "max-1350": "1350px", // Applies when width <= 1350px
+        "max-1285": "1285px", // Applies when width <= 1285px
+        "max-1257": "1257px", // Applies when width <= 1257px
+        "max-1080": "1080px", // Applies when width <= 1080px
+        "max-950": "950px",   // Applies when width <= 950px
+        "max-802": "802px",   // Applies when width <= 802px
+        "max-750": "750px",   // Applies when width <= 750px
+        "max-650": "650px",   // Applies when width <= 650px
+        "max-550": "550px",   // Applies when width <= 550px
+        "max-450": "450px",   // Applies when width <= 450px
+        "max-415": "415px",   // Applies when width <= 415px
       },
     },
   },
